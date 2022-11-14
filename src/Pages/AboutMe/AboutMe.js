d6bf78dedd31dd027c4e9b6c1a0258f35b3d942c
ggnaz <nazersomera123@gmail.com>
2022-11-14 20:36:06 +0700

    saved

--- src/Pages/AboutMe/AboutMe.js
+++ src/Pages/AboutMe/AboutMe.js
@@ -28,31 +28,56 @@ const AboutMe = () => {
       variants={cardVariants}
       initial="offscreen"
     >
-     <div className="flex flex-row justify-center  min-h-screen  lg:flex-row md:flex-row sm:flex-col xs:flex-col">
-        <div className="flex sm:justify-center xs:justify-center items-center">
-          
-        <img
+       <div className="hero min-h-screen bg-base-100 overflow-x-hidden">
+        <div className="hero-content flex-col lg:flex-row">
+          {/* <div className="lg:w-1/2 lg:mr-16 lg:flex lg:justify-center lg:items-center"> */}
+            <div className="lg:w-96 relative items-center">
+              {/* <div className="absolute pattern-dots-xl text-blue-100 top-0 left-0 w-32 h-48 md:h-96 transform -translate-y-12 -translate-x-16 -rotate-3"></div>
+              <div className="absolute pattern-dots-xl text-blue-100 bottom-0 right-0 w-32 h-48 md:h-96 transform translate-y-12 translate-x-16 rotate-3"></div>
+              <div className="absolute rounded-full top-0 right-0 w-32 h-32 bg-yellow-200 bg-opacity-50 -mt-12 -mr-12"></div>
+              <div className="absolute rounded-xl bottom-0 left-0 w-32 h-32 bg-blue-200 bg-opacity-50 -mb-10 -ml-10 transform rotate-3"></div> */}
+              {/* <img src="https://source.unsplash.com/MChSQHxGZrQ/800x1000" alt="Hero Image" class="relative rounded-lg mx-auto shadow-lg"> */}
+              <div className="">
+                <img
                 
-                src={nazerimg}
-                alt="nazerimg"
-                className="max-w-sm rounded-lg shadow-2xl max-h-96 items-center mx-5"
-              />
-        </div>
-        <div className="flex flex-col mx-5 justify-center ">
-           
-             <h1 className="text-5xl font-bold ">About Me</h1>
-           
+                  src={nazerimg}
+                  alt="nazerimg"
+                  className="mask mask-squircle max-w-sm rounded-lg shadow-2xl"
+                />
+              </div>
+            </div>
+          {/* </div> */}
+
+          <div className="mt-8">
+            <h1 className="text-5xl font-bold ">About Me</h1>
             <p className="py-6">
-              Hi! I'm Nazer and i love creating things that live on the
+            Hi! I'm Nazer and i love creating things that live on the
               internet. My interest in web development started way back in 2018
               when i was a college student. Im currently focusing Front End development and I already know how to use API's and how 
               to integrate from the backend services to user interface (UI).
             </p>
-              
+           
+            <button className="btn btn-outline btn-blue-100 gap-2">
+              View resume{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                />
+              </svg>
+            </button>
           </div>
+        </div>
+      </div>
 
-     </div>
-      
     </motion.div>
   );
 };
